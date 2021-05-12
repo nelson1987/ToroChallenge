79e39c26ff23f7fccc9660740779633d7646041d
Nelson Neto <nelson.ash@outlook.com>
2021-05-12 09:33:51 -0300

    Inclusao de Repository utilizando Abstract Factory

--- src/Repositories/ToroChallenge.Repository/ContaCorrenteRepository.cs
+++ src/Repositories/ToroChallenge.Repository/ContaCorrenteRepository.cs
@@ -2,7 +2,46 @@
 
 namespace ToroChallenge.Repository
 {
-    public class ContaCorrenteRepository
+    public class ContaCorrenteRepository : IRepository<object>
     {
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetByFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RepositoryMySql<TEntity> : IRepositoryMySql<Object>
+    {
+        public void Update()
+        {
+        }
+        public void Delete()
+        {
+        }
+    }
+
+    public class RepositoryMongo<TEntity> : IRepositoryMongo<Object>
+    {
+        public void GetByFilter()
+        {
+        }
+        public void Insert()
+        {
+        }
     }
 }
