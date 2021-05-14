2ba79b859a930533c04e950b268d9e1d12ee19bf
nelson1987 <nelson.ash@outlook.com>
2021-05-13 22:35:18 -0300

    Alteração de Carteira Tests

--- src/Dominio/ToroChallenge.PapelContexto.Domain.Tests/CarteiraTests.cs
+++ src/Dominio/ToroChallenge.PapelContexto.Domain.Tests/CarteiraTests.cs
@@ -11,12 +11,12 @@ namespace ToroChallenge.PapelContexto.Domain.Tests
         {
         }
 
-        [TestCase("", "SANB11", 3, true, "Conta", Category = "CONTA_DEVE_ESTAR_PREENCHIDO_SENAO_ORDEM_SERA_INVALIDADA")]
-        [TestCase(null, "SANB11", 3, true, "Conta", Category = "CONTA_DEVE_ESTAR_PREENCHIDO_SENAO_ORDEM_SERA_INVALIDADA")]
-        [TestCase("1234", "", 3, true, "Papel", Category = "PAPEL_DEVE_ESTAR_PREENCHIDO_SENAO_ORDEM_SERA_INVALIDADA")]
-        [TestCase("1234", null, 3, true, "Papel", Category = "PAPEL_DEVE_ESTAR_PREENCHIDO_SENAO_ORDEM_SERA_INVALIDADA")]
-        [TestCase("1234", "SANB11", 0, true, "Quantidade", Category = "QUANTIDADE_DEVE_ESTAR_PREENCHIDO_E_SER_MAIOR_QUE_ZERO_SENAO_ORDEM_SERA_INVALIDADA")]
-        [TestCase("1234", "SANB11", null, true, "Quantidade", Category = "QUANTIDADE_DEVE_ESTAR_PREENCHIDO_E_SER_MAIOR_QUE_ZERO_SENAO_ORDEM_SERA_INVALIDADA")]
+        [TestCase("", "SANB11", 3, false, "Conta", Category = "CONTA_DEVE_ESTAR_PREENCHIDO_SENAO_ORDEM_SERA_INVALIDADA")]
+        [TestCase(null, "SANB11", 3, false, "Conta", Category = "CONTA_DEVE_ESTAR_PREENCHIDO_SENAO_ORDEM_SERA_INVALIDADA")]
+        [TestCase("1234", "", 3, false, "Papel", Category = "PAPEL_DEVE_ESTAR_PREENCHIDO_SENAO_ORDEM_SERA_INVALIDADA")]
+        [TestCase("1234", null, 3, false, "Papel", Category = "PAPEL_DEVE_ESTAR_PREENCHIDO_SENAO_ORDEM_SERA_INVALIDADA")]
+        [TestCase("1234", "SANB11", 0, false, "Quantidade", Category = "QUANTIDADE_DEVE_ESTAR_PREENCHIDO_E_SER_MAIOR_QUE_ZERO_SENAO_ORDEM_SERA_INVALIDADA")]
+        [TestCase("1234", "SANB11", null, false, "Quantidade", Category = "QUANTIDADE_DEVE_ESTAR_PREENCHIDO_E_SER_MAIOR_QUE_ZERO_SENAO_ORDEM_SERA_INVALIDADA")]
         [TestCase("1234", "SANB11", 3, true, "Valor", Category = "ORDEM_VALIDA")]
         public void RealizarOrdem(string conta, string papel, int quantidade, bool valida, string propriedadeErro)
         {
